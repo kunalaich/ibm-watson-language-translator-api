f9c25c71e582aaeea261561a63b2c1a27df9a241
Kunal Aich <kunalaich.silchar@gmail.com>
2021-02-05 00:17:40 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,11 +1,17 @@
 import unittest
 
-from translator import englishtofrench
+from translator import englishtofrench, englishtogerman
 
 class TestEnglishToFrench(unittest.TestCase): 
     def test_englishtofrench(self): 
         self.assertEqual(englishtofrench(""), "Input is empty")
         self.assertEqual(englishtofrench("Hello"), "Bonjour")
         self.assertNotEqual(englishtofrench("Hey"), "Bonjour")
+
+class TestEnglishToGerman(unittest.TestCase): 
+    def test_englishtogerman(self): 
+        self.assertEqual(englishtogerman(""), "Input is empty")
+        self.assertEqual(englishtogerman("Hello"), "Hallo")
+        self.assertNotEqual(englishtogerman("Hey"), "Hallo")
 
 unittest.main()
